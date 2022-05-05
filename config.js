6b6a1478ebe5a100319acaa416472a9a97ff3153
velvet-facade <104369167+velvet-facade@users.noreply.github.com>
2022-05-05 22:40:49 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,21 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+//Paste your firebaseConfig here
+const firebaseConfig = {
+  apiKey: "AIzaSyAvAXLrGWhdjQ-1rCA6Up4HCttfuDXt_Is",
+  authDomain: "pro-c78-e-ride-stage-4.firebaseapp.com",
+  projectId: "pro-c78-e-ride-stage-4",
+  storageBucket: "pro-c78-e-ride-stage-4.appspot.com",
+  messagingSenderId: "283629721465",
+  appId: "1:283629721465:web:0a806ef675f0e8c273c1a8"
+};
+
+
+
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
+
+
